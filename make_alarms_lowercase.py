c0e2d5c51451d2111f6910989c44376955f2b503
Dwayne Sutherland <dwayne@dwaynes-mbp.home>
2022-06-01 13:17:06 +0100

    added logging to alarm retrival function

--- make_alarms_lowercase.py
+++ make_alarms_lowercase.py
@@ -2,12 +2,20 @@ import sys
 
 import boto3
 import colored
+import logging
 from colored import stylize
 
 
+
+
 def get_metric_alarms_by_prefix(client, prefix:str) -> list:
-  alarms = client.describe_alarms(AlarmNamePrefix=prefix)
-  return alarms.get("MetricAlarms", [])
+  try:
+    alarms = client.describe_alarms(AlarmNamePrefix=prefix)
+    logging.info(f'Gathered all alarms matching prefix: {prefix}')
+    return alarms.get("MetricAlarms", [])
+  except Exception as error:
+    logging.error(f"Unable to gather alarms. error: {error}")
+    exit()
 
 def rename_metric_alarm_lowercase(client, alarm:str) -> None:
   try:
@@ -42,15 +50,17 @@ def read_arguments() -> dict[str, str]:
 
 
 if __name__ == '__main__':
+  # setup
+  logging.basicConfig(filename='make_alarms_lowecase.log', format='%(asctime)s - %(levelname)s - %(message)s')
   prefix, region = read_arguments()
   client = boto3.client('cloudwatch', region_name=region)
 
-  # Gather alarms an seperates out the uppercase alarms
+  # Gather alarms and seperates out the uppercase alarms
   uppercase_alarms = [alarm for alarm in get_metric_alarms_by_prefix(client, prefix) if alarm['AlarmName'] != alarm['AlarmName'].lower() ]
 
   # exits script if no uppercase alarms are found
   if len(uppercase_alarms) == 0:
-    print('No alarms to modify')
+    print('No uppercase alarms to modify')
     exit() 
 
   # Display alarms and request confirmation before continuing script 
